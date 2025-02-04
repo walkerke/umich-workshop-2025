7b4e44816c0b83f17df9f9924e517256b6c8f773
Kyle Walker <kyle@walker-data.com>
2025-02-04 14:53:24 -0600

    update code

--- acs-2023/acs-2023-code.R
+++ acs-2023/acs-2023-code.R
@@ -1,19 +1,19 @@
 options(tigris_use_cache = TRUE)
 
-## install.packages(c("tidycensus", "tidyverse"))
+# install.packages(c("tidycensus", "tidyverse"))
 
-## install.packages(c("mapview", "survey", "srvyr"))
+# install.packages(c("mapview", "survey", "srvyr", "arcgislayers"))
 
-## library(tidycensus)
-## 
-## census_api_key("YOUR KEY GOES HERE", install = TRUE)
+# library(tidycensus)
+# 
+# census_api_key("YOUR KEY GOES HERE", install = TRUE)
 
 library(tidycensus)
 
 median_value <- get_acs(
   geography = "county",
   variables = "B25077_001",
-  year = 2022
+  year = 2023
 )
 
 median_value
@@ -21,7 +21,7 @@ median_value
 median_value_1yr <- get_acs(
   geography = "place",
   variables = "B25077_001",
-  year = 2022,
+  year = 2023,
   survey = "acs1"
 )
 
@@ -30,7 +30,7 @@ median_value_1yr
 income_table <- get_acs(
   geography = "county", 
   table = "B19001", 
-  year = 2022
+  year = 2023
 )
 
 income_table
@@ -40,20 +40,20 @@ sd_value <- get_acs(
   variables = "B25077_001", 
   state = "CA", 
   county = "San Diego",
-  year = 2022
+  year = 2023
 )
 
 sd_value
 
-## vars <- load_variables(2022, "acs5")
-## 
-## View(vars)
-## 
+# vars <- load_variables(2023, "acs5")
+# 
+# View(vars)
+# 
 
 age_sex_table <- get_acs(
   geography = "state", 
   table = "B01001", 
-  year = 2022,
+  year = 2023,
   survey = "acs1",
 )
 
@@ -63,7 +63,7 @@ age_sex_table
 age_sex_table_wide <- get_acs(
   geography = "state", 
   table = "B01001", 
-  year = 2022,
+  year = 2023,
   survey = "acs1",
   output = "wide" 
 )
@@ -76,7 +76,7 @@ ca_education <- get_acs(
   variables = c(percent_high_school = "DP02_0062P", 
                 percent_bachelors = "DP02_0065P",
                 percent_graduate = "DP02_0066P"), 
-  year = 2021
+  year = 2023
 )
 
 ca_education
@@ -84,14 +84,14 @@ ca_education
 get_acs(
   geography = "state",
   variables = "B16001_054",
-  year = 2022,
+  year = 2023,
   survey = "acs1"
 )
 
 get_acs(
   geography = "state",
   variables = "B16001_054",
-  year = 2022,
+  year = 2023,
   survey = "acs5"
 )
 
@@ -99,7 +99,7 @@ utah_income <- get_acs(
   geography = "county",
   variables = "B19013_001",
   state = "UT",
-  year = 2022
+  year = 2023
 ) 
 
 library(ggplot2)
@@ -125,7 +125,7 @@ utah_plot <- utah_plot +
 utah_plot
 
 utah_plot <- utah_plot + 
-  labs(title = "Median household income, 2018-2022 ACS",
+  labs(title = "Median household income, 2019-2023 ACS",
        subtitle = "Counties in Utah",
        caption = "Data acquired with R and tidycensus",
        x = "ACS estimate",
@@ -134,7 +134,7 @@ utah_plot <- utah_plot +
 
 utah_plot
 
-## View(utah_income)
+# View(utah_income)
 
 utah_plot_errorbar <- ggplot(utah_income, aes(x = estimate, 
                                         y = reorder(NAME, estimate))) + 
@@ -143,7 +143,7 @@ utah_plot_errorbar <- ggplot(utah_income, aes(x = estimate,
   geom_point(color = "darkblue", size = 2) + 
   scale_x_continuous(labels = label_dollar()) + 
   scale_y_discrete(labels = function(x) str_remove(x, " County, Utah")) + 
-  labs(title = "Median household income, 2018-2022 ACS",
+  labs(title = "Median household income, 2019-2023 ACS",
        subtitle = "Counties in Utah",
        caption = "Data acquired with R and tidycensus. Error bars represent margin of error around estimates.",
        x = "ACS estimate",
@@ -157,149 +157,147 @@ cook_education <- get_acs(
   variables = "DP02_0068P",
   state = "IL",
   county = "Cook",
-  year = 2022,
-  geometry = TRUE
+  year = 2023
 )
 
-cook_education
+library(arcgislayers)
+
+cook_geo <- arc_read("https://tigerweb.geo.census.gov/arcgis/rest/services/Generalized_ACS2023/Tracts_Blocks/MapServer/4", where = "COUNTY = '031' AND STATE = '17'")
+
+cook_geo
+
+library(dplyr)
+
+cook_education_geo <- cook_geo %>%
+  select(GEOID) %>%
+  left_join(cook_education, by = "GEOID")
+
+cook_education_geo
 
 library(mapview)
 
-mapview(cook_education)
+mapview(cook_education_geo)
 
-mapview(cook_education, zcol = "estimate")
+mapview(cook_education_geo, zcol = "estimate")
 
 tx_education <- get_acs(
   geography = "county",
   variables = "DP02_0068P",
   state = "TX",
-  year = 2022,
-  survey = "acs1",
-  geometry = TRUE
+  year = 2023,
+  survey = "acs1"
 )
 
-mapview(tx_education, zcol = "estimate")
+tx_counties <- arc_read("https://tigerweb.geo.census.gov/arcgis/rest/services/Generalized_ACS2023/State_County/MapServer/11", where = "STATE = '48'")
 
-wa_wfh <- get_acs(
+tx_geo <- left_join(tx_counties, tx_education, by = "GEOID")
+
+mapview(tx_geo, zcol = "estimate")
+
+tx_wfh <- get_acs(
   geography = "puma",
   variables = "DP03_0024P",
-  state = "WA",
+  state = "TX",
   survey = "acs1",
-  year = 2022,
-  geometry = TRUE
+  year = 2023
 )
+
+tx_pumas <- arc_read("https://tigerweb.geo.census.gov/arcgis/rest/services/Generalized_TAB2020/PUMA_TAD_TAZ_UGA_ZCTA/MapServer/4", where = "STATE = '48'")
+
+tx_geo <- left_join(tx_pumas, tx_wfh, by = "GEOID")
 
 library(mapview)
 
-mapview(wa_wfh, zcol = "estimate")
-
-ct_income <- get_acs(
-  geography = "county",
-  variables = "B19013_001",
-  state = "CT",
-  year = 2022,
-  survey = "acs1",
-  geometry = TRUE
-)
-
-mapview(ct_income, zcol = "estimate")
-
-
-library(tigris)
-library(mapview)
-ct_tract_21 <- tracts("CT", cb = TRUE, year = 2021)
-ct_tract_22 <- tracts("CT", cb = TRUE, year = 2022)
-
-mapview(ct_tract_21) | mapview(ct_tract_22)
+mapview(tx_geo, zcol = "estimate")
 
 library(tidycensus)
 
-or_pums <- get_pums(
+la_pums <- get_pums(
   variables = c("SEX", "AGEP", "HHT"),
-  state = "OR",
+  state = "LA",
   survey = "acs1",
-  year = 2022
+  year = 2023
 )
 
-or_pums
+la_pums
 
 library(tidyverse)
 
-or_age_40 <- filter(or_pums, AGEP == 40)
+la_age_41 <- filter(la_pums, AGEP == 41)
 
-print(sum(or_pums$PWGTP))
-print(sum(or_age_40$PWGTP))
+print(sum(la_pums$PWGTP))
+print(sum(la_age_41$PWGTP))
 
-get_acs("state", "B01003_001", state = "OR", survey = "acs1", year = 2022)
+get_acs("state", "B01003_001", state = "LA", survey = "acs1", year = 2023)
 
-## View(pums_variables)
+# View(pums_variables)
 
-or_pums_recoded <- get_pums(
+la_pums_recoded <- get_pums(
   variables = c("SEX", "AGEP", "HHT"),
-  state = "OR",
+  state = "LA",
   survey = "acs1",
-  year = 2022,
+  year = 2023,
   recode = TRUE
 )
 
-or_pums_recoded
+la_pums_recoded
 
-or_pums_filtered <- get_pums(
+la_pums_filtered <- get_pums(
   variables = c("SEX", "AGEP", "HHT"),
-  state = "OR",
+  state = "LA",
   survey = "acs5",
   variables_filter = list(
     SEX = 2,
     AGEP = 30:49
   ),
-  year = 2022
+  year = 2023
 )
 
-or_pums_filtered
+la_pums_filtered
 
-or_age_by_puma <- get_pums(
+la_age_by_puma <- get_pums(
   variables = c("PUMA", "AGEP"),
-  state = "OR",
+  state = "LA",
   survey = "acs1",
-  year = 2022
+  year = 2023
 )
 
-or_age_by_puma
+la_age_by_puma
 
-or_pums_replicate <- get_pums(
+la_pums_replicate <- get_pums(
   variables = c("AGEP", "PUMA"),
-  state = "OR",
+  state = "LA",
   survey = "acs1",
-  year = 2022,
+  year = 2023,
   rep_weights = "person" 
 )
 
 
-or_pums_replicate
+la_pums_replicate
 
-or_survey <- to_survey(
-  or_pums_replicate,
+la_survey <- to_survey(
+  la_pums_replicate,
   type = "person"
 )
 
-class(or_survey)
+class(la_survey)
 
 library(srvyr)
 
-or_survey %>%
-  filter(AGEP == 40) %>%
+la_survey %>%
+  filter(AGEP == 41) %>%
   survey_count() %>%
   mutate(n_moe = n_se * 1.645)
 
-or_survey %>%
+la_survey %>%
   group_by(PUMA) %>%
   summarize(median_age = survey_median(AGEP)) %>%
   mutate(median_age_moe = median_age_se * 1.645)
 
-or_age_puma <- get_acs(
+la_age_puma <- get_acs(
   geography = "puma",
   variables = "B01002_001",
-  state = "OR",
-  year = 2022,
+  state = "LA",
+  year = 2023,
   survey = "acs1"
 )
